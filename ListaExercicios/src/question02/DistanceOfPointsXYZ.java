f2a7e7e26ba41fdcc2b699ae6ac567b010cf0e8a
hugosilva-dev <eahugosilva@gmail.com>
2023-06-09 16:42:31 -0300

    Learning UML Classes Diagram

---
+++ ListaExercicios/src/question02/DistanceOfPointsXYZ.java
@@ -0,0 +1,29 @@
+package question02;
+
+import java.util.Scanner;
+
+public class DistanceOfPointsXYZ {
+    public static void main(String[] args) {
+        Scanner reading = new Scanner(System.in);
+        System.out.print("Please enter the value of A point in format (x,y,z): ");
+        String pointA = reading.nextLine();
+        System.out.print("Please enter the value of B point in format (x,y,z): ");
+        String pointB = reading.nextLine();
+
+        String[] coordenadasA = pointA.split(",");
+        String[] coordenadasB = pointB.split(",");
+
+        double xA = Double.parseDouble(coordenadasA[0]);
+        double yA = Double.parseDouble(coordenadasA[1]);
+        double zA = Double.parseDouble(coordenadasA[2]);
+        double xB = Double.parseDouble(coordenadasB[0]);
+        double yB = Double.parseDouble(coordenadasB[1]);
+        double zB = Double.parseDouble(coordenadasB[2]);
+
+        Double distance = Math.sqrt(
+                (Math.pow((xB - xA), 2)) + (Math.pow((yB - yA), 2)) + (Math.pow((zB - zA), 2))
+        );
+        System.out.printf("Distance = %.2f", distance);
+
+    }
+}
